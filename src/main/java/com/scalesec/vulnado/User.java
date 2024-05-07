0755a5fbd2438dff424ada670b8c8e5e4a28087f
JvIadEye <135623930+JvIadEye@users.noreply.github.com>
2024-05-07 11:50:00 +0700

    Update User.java

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -21,7 +21,7 @@ public class User {
   public String token(String secret) {
     SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
     String jws = Jwts.builder().setSubject(this.username).signWith(key).compact();
-    return jws;
+//    return jws;
   }
 
   public static void assertAuth(String secret, String token) {
